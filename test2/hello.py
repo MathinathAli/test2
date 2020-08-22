4197f838b04904906445cc70a9e5120da5fb28b7
Mathinath Ali <Mathinath.Ali@gds.ey.com>
2020-08-22 08:57:04 +0530

    Added home path

--- test2/hello.py
+++ test2/hello.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # To add other resources, create functions that generate the page contents
 # and add decorators to define the appropriate resource locators for them.
 
-#@app.route('/')
+@app.route('/')
 @app.route('/hello')
 def hello():
     # Render the page
